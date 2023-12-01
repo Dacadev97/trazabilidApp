ef2eeccfe5a38a8f78c264c3aa27397f5e7a7135
DakaAlvarez97 <davidson.cadavid@est.iudigital.edu.co>
2023-12-01 17:10:46 -0500

    Update environment variables and CSS media queries

--- resources/views/entregas/show.blade.php
+++ resources/views/entregas/show.blade.php
@@ -2,6 +2,7 @@
 <html lang="es">
 <head>
       <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>Detalles de Entrega</title>
       <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
       <link href="{{ asset('css/app.css') }}" rel="stylesheet">
